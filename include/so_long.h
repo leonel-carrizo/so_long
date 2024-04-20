67d55ea0df625188d0fb4223d1aacc42a6ab96ce
Leonel  Carrizo <lcarrizo@student.42london.com>
2024-04-20 20:27:53 +0100

    add preliminar estructure to the project

---
+++ include/so_long.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcarrizo <lcarrizo@student.42london.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 16:05:04 by lcarrizo          #+#    #+#             */
+/*   Updated: 2024/04/20 20:16:43 by lcarrizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../lib/libft/include/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+
+
+#endif
