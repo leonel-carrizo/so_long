5f780e0f645306112ee52346751509ae8c9af074
Leonel  Carrizo <lcarrizo@student.42london.com>
2024-04-22 21:56:26 +0100

    add minilibx to header & math.h

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: lcarrizo <lcarrizo@student.42london.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 16:05:04 by lcarrizo          #+#    #+#             */
-/*   Updated: 2024/04/20 20:16:43 by lcarrizo         ###   ########.fr       */
+/*   Updated: 2024/04/22 18:39:36 by lcarrizo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 # define MINITALK_H
 
 # include "../lib/libft/include/libft.h"
+# include "../lib/libmlx/mlx.h"
 # include <unistd.h>
 # include <stdlib.h>
+# include <math.h>
 
 
 #endif
