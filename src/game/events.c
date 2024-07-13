b9263aec8a1373f070eecbfbab6f71be93b16638
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-07-14 00:14:44 +0100

    refactor: move key events to events.c file

---
+++ src/game/events.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcarrizo <lcarrizo@student.42london.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 00:10:23 by lcarrizo          #+#    #+#             */
+/*   Updated: 2024/07/14 00:10:48 by lcarrizo         ###    ###london.com    */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/so_long.h"
+
+/* Checks if the player's move is valid */
+int	is_valid_move(t_map map, int new_x, int new_y, int keycode)
+{
+	if (keycode == A || keycode == W || keycode == D || keycode == S
+		|| keycode == LEFT || keycode == RIGHT
+		|| keycode == UP || keycode == DOWN)
+	{
+		if (keycode == A || keycode == LEFT)
+			new_x--;
+		else if (keycode == W || keycode == UP)
+			new_y--;
+		else if (keycode == D || keycode == RIGHT)
+			new_x++;
+		else if (keycode == S || keycode == DOWN)
+			new_y++;
+		if (new_x < 0 || new_y < 0 || new_x >= map.width || new_y >= map.height)
+			return (0);
+		if (map.tiles[new_y][new_x] == '1')
+			return (0);
+		if (map.tiles[new_y][new_x] == 'E' && !map.exit.is_open)
+			return (0);
+		return (1);
+	}
+	return (0);
+}
+
+/* Updates the player's position based on the key pressed */
+void	update_player_position(t_game *game, int keycode)
+{
+	if (keycode == A || keycode == LEFT)
+		game->player.position.x--;
+	else if (keycode == W || keycode == UP)
+		game->player.position.y--;
+	else if (keycode == D || keycode == RIGHT)
+		game->player.position.x++;
+	else if (keycode == S || keycode == DOWN)
+		game->player.position.y++;
+	game->player.n_moves++;
+	ft_printf("No. movements: %d\n", game->player.n_moves);
+}
+
+/* Handles keyboard events */
+int	key_press(int keycode, t_game *game)
+{
+	int	new_x;
+	int	new_y;
+
+	if (keycode == ESC)
+		exit_game(game);
+	new_x = game->player.position.x;
+	new_y = game->player.position.y;
+	if (is_valid_move(game->map, new_x, new_y, keycode))
+	{
+		update_player_position(game, keycode);
+		update_map(game, new_x, new_y);
+		draw_map(game, new_x, new_y);
+	}
+	if (game->player.won)
+		exit_game(game);
+	return (0);
+}
